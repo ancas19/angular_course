1a76ad93341f665f8399ee192f4a63e10c820057
afcastro <afcastro@cotrafa.com.co>
2025-06-07 13:00:41 -0500

    Feat: Continue with the course using services

--- 03-gifs-app/src/environments/environment.development.ts
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -2,5 +2,7 @@ export const environment = {
     production:true,
     companyName: 'Gifs',
     companyName2:'App',
-    companySlogan:'Maneja tus GIFs favoritos'
+    companySlogan:'Maneja tus GIFs favoritos',
+    giphyApiKey: 'MSc7XwFAHIDEQE8EIHrcN4v8laa5Xl6a',
+    giphyUrl:'https://api.giphy.com/v1'
 };
