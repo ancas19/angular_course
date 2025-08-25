a768fd357de5a7253e140752f0c433007b14e09f
ancas19 <andres.cmonsalve@gmail.com>
2025-08-24 22:55:34 -0500

    Feat: Continue with cours's final project

---
+++ 10-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  imports: [],
+  template:``
+})
+export class HeavyLoadersComponentFast { }
---
+++ 10-my-dashboard/src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-loader',
+  imports:[],
+  template: ``
+})
+
+export class UsersLoaderComponent {
+
+}
